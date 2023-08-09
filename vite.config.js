07bc6e660fa398d7be2544d4503fe34a6cd2e7c1
abc12345d <zhiqi@Zhis-MacBook-Pro.local>
2023-08-09 20:53:57 +0100

    add code to deploy repo on github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "portfolio_website",
   plugins: [svgr(), react()],assetsInclude: ['**/*.md']
   
 });

dbadb3986050395ef35810b44cd121a9cf117a4a
abc12345d <zhiqi31@gmail.com>
2023-08-09 21:43:37 +0100

    change repo name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "portfolio_website",
+  base: "abc12345d.github.io",
   plugins: [svgr(), react()],assetsInclude: ['**/*.md']
   
 });

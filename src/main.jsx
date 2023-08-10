858cc5d46cb5d10585d5f86ba100c997d734d1a4
abc12345d <zhiqi31@gmail.com>
2023-08-10 20:53:23 +0100

    add basename to react router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={"/myPortfolio/"}>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/myPortfolio/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>

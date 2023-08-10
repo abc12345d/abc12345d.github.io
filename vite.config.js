e346380619d20320f09e943b8fa39d3b09da6a4a
abc12345d <zhiqi31@gmail.com>
2023-08-10 21:45:10 +0100

    Build successfully!

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/myPortfolio/",
+  // base: "/myPortfolio/",
   plugins: [svgr(), react()],assetsInclude: ['**/*.md']
   
 });

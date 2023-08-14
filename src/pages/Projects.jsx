8ecf34f0ecdc40f88aea455758d41287127a257c
abc12345d <zhiqi31@gmail.com>
2023-08-14 17:28:57 +0100

    add gif of various projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,16 +5,42 @@ const filename = "image_ipsum";
 const Projects = () => (
   <div className="cards">
     <ProjectCard
-      projectName={"Snake_Game"}
-      description={"A Python-powered classic"}
+      projectName={"Stock_News_Alert_System"}
+      description={
+        "Sends SMS notifications when the closing price of yesterday and the day before yesterday differs by more than 5%"
+      }
       fileExtension={"gif"}
     />
     <ProjectCard
-      projectName={filename}
+      projectName={"Personalised_Email_Automation"}
       description={
-        "dsefsdfvh hihi ijijij huihjijiji jijij  dsfvdsfsdfsdfewrwefewfwe"
+        "Sends bulk of personalised emails automatically from CSV file using Python."
       }
-      fileExtension={"png"}
+      fileExtension={"gif"}
+    />
+    <ProjectCard
+      projectName={"Snake_Game"}
+      description={"A classic Snake game empowered by Python."}
+      fileExtension={"gif"}
+    />
+    <ProjectCard
+      projectName={"Pong_Game"}
+      description={"An iconic two-player pong game powered by Python."}
+      fileExtension={"gif"}
+    />
+    <ProjectCard
+      projectName={"Pomodoro_Timer"}
+      description={
+        "A simple GUI pomodoro timer implemented using Python Tkinter module."
+      }
+      fileExtension={"gif"}
+    />
+    <ProjectCard
+      projectName={"Turtle_Crossing_Game"}
+      description={
+        "A Python-powered simplified version of classic Frogger game."
+      }
+      fileExtension={"gif"}
     />
   </div>
 );

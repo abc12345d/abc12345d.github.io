2b1ce2226ee8e4dac16b4223331398b7648defe4
abc12345d <zhiqi31@gmail.com>
2023-08-27 23:16:34 +0100

    try fixing code highlight

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -1,11 +1,11 @@
-import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 function Code({ className, children }) {
   const language = className.replace("lang-", "");
   return (
     <div className="codeBlock">
-      <SyntaxHighlighter language={language} style={materialDark}>
+      <SyntaxHighlighter language={language.toLowerCase()} style={materialDark}>
         {children}
       </SyntaxHighlighter>
     </div>

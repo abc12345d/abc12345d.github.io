4f5eec3c2db7d94b3eb3a8f88b40025288cb787e
abc12345d <zhiqi31@gmail.com>
2023-08-27 23:23:48 +0100

    change code highlight style

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -1,11 +1,11 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 function Code({ className, children }) {
   const language = className.replace("lang-", "");
   return (
     <div className="codeBlock">
-      <SyntaxHighlighter language={language.toLowerCase()} style={dracula}>
+      <SyntaxHighlighter language={language.toLowerCase()} style={materialDark}>
         {children}
       </SyntaxHighlighter>
     </div>
